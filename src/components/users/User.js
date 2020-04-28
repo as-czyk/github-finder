af72f305f2a8d26ffe4f97c8a21c8a28f58847e2
czyk <aron.scheffczyk@gmail.com>
2020-04-28 10:38:44 +0200

    added Spinner, User, Search and Useritem component

---
+++ src/components/users/User.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Useritem from './Useritem';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
+
+const User = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
+    return (
+      <div style={userStyle}>
+        {users.map((user) => (
+          <Useritem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  }
+};
+
+User.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
+
+const userStyle = {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3, 1fr)',
+  gridGap: '1rem',
+};
+
+export default User;
