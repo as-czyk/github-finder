9917f0502d0bb6ab1b28cb34356b0c676beb0bd6
czyk <aron.scheffczyk@gmail.com>
2020-04-28 10:39:41 +0200

    set up navbar, alert component

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import User from './components/users/User';
+import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
+import axios from 'axios';
+import './App.css';
+
+class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+    alert: null,
+  };
+
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  clearUsers = () => {
+    this.setState({ users: [], loading: false });
+  };
+
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+    setTimeout(() => {
+      this.setState({ alert: null });
+    }, 5000);
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <Navbar />
+        <div className='container'>
+          <Alert alert={this.state.alert} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={this.state.users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+          />
+          <User loading={this.state.loading} users={this.state.users} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
